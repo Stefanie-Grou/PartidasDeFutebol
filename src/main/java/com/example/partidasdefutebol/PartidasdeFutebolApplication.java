9490b892b0047142680c4bbf1483d24c0a9518e1
Stefanie-Grou <stefanie.limacastro@yahoo.com>
2025-06-27 14:55:47 -0300

    Primeiro commit

---
+++ src/main/java/com/example/partidasdefutebol/PartidasdeFutebolApplication.java
@@ -0,0 +1,13 @@
+package com.example.partidasdefutebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartidasdeFutebolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartidasdeFutebolApplication.class, args);
+        System.out.println("Hi");
+    }
+}
---
+++ src/test/java/com/example/partidasdefutebol/PartidasdeFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partidasdefutebol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidasdeFutebolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
