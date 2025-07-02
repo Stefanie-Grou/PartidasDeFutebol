c8825ca25c8222392a7eff4f877de16ed0ecc1d1
Stefanie-Grou <stefanie.limacastro@yahoo.com>
2025-07-02 12:21:07 -0300

    Feature: adicionando a possibilidade de editar os clubes.

--- src/main/java/com/example/partidasdefutebol/controller/ClubController.java
+++ src/main/java/com/example/partidasdefutebol/controller/ClubController.java
@@ -5,15 +5,12 @@ import com.example.partidasdefutebol.repository.ClubRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/criar-clube")
+@RequestMapping("/clube")
 public class ClubController {
     @Autowired
     private ClubRepository clubRepository;
@@ -23,7 +20,32 @@ public class ClubController {
         if (clubRepository.existsByClubNameAndStateAcronym(club.getClubName(), club.getStateAcronym())) {
             return ResponseEntity.status(409).build();
         }
-        Club savedClub = clubRepository.save(club);
-        return ResponseEntity.status(201).body(savedClub);
+
+        try {
+            Club savedClub = clubRepository.save(club);
+            return ResponseEntity.status(201).body(savedClub);
+        } catch (Exception e) {
+            return ResponseEntity.status(409).build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Club requestedToUpdateClub) {
+        Optional<Club> clubOptional = clubRepository.findById(id);
+        if (clubOptional.isEmpty()) {
+            return ResponseEntity.status(404).build();
+        }
+
+        Club existingClub = clubOptional.get();
+        existingClub.setClubName(requestedToUpdateClub.getClubName());
+        existingClub.setStateAcronym(requestedToUpdateClub.getStateAcronym());
+        existingClub.setCreatedOn(requestedToUpdateClub.getCreatedOn());
+        existingClub.setIsActive(requestedToUpdateClub.getIsActive());
+        try {
+            Club updatedClub = clubRepository.save(existingClub);
+            return ResponseEntity.status(200).body(updatedClub);
+        } catch (Exception e) {
+            return ResponseEntity.status(409).build();
+        }
     }
 }
--- src/main/java/com/example/partidasdefutebol/service/ClubService.java
+++ src/main/java/com/example/partidasdefutebol/service/ClubService.java
@@ -3,6 +3,8 @@ package com.example.partidasdefutebol.service;
 import com.example.partidasdefutebol.repository.Club;
 import com.example.partidasdefutebol.repository.ClubRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +12,12 @@ public class ClubService {
 
     @Autowired
     private static ClubRepository clubRepository;
+
     public Club save(Club club) {
         return clubRepository.save(club);
+    }
+
+    public Club update(Long id, Club requestedToUpdateClub) {
+        return clubRepository.save(requestedToUpdateClub);
     }
 }
