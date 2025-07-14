0e353219b4a99511827869d8bd40bbcc018676be
Stefanie-Grou <stefanie.limacastro@yahoo.com>
2025-07-14 18:05:25 -0300

    Adicionando a funcionalidade de edição de partidas utilizando o ID, via PUT

--- src/main/java/com/example/partidasdefutebol/controller/MatchController.java
+++ src/main/java/com/example/partidasdefutebol/controller/MatchController.java
@@ -1,31 +1,42 @@
 package com.example.partidasdefutebol.controller;
 
 import com.example.partidasdefutebol.entities.MatchEntity;
+import com.example.partidasdefutebol.entities.StadiumEntity;
 import com.example.partidasdefutebol.service.MatchService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping
+@RequestMapping("/partida")
 public class MatchController {
 
     @Autowired
     private MatchService matchService;
 
-    @PostMapping("/partida")
+    @PostMapping
     public ResponseEntity<MatchEntity> createMatch(@Valid @RequestBody MatchEntity matchEntity) {
         try {
             MatchEntity savedMatchEntity = matchService.createMatch(matchEntity);
             return ResponseEntity.status(201).body(savedMatchEntity);
         } catch (ResponseStatusException ex) {
-           throw new ResponseStatusException(HttpStatus.CONFLICT);
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping
+    @RequestMapping("/{id}")
+    public ResponseEntity<MatchEntity> updateMatch(@PathVariable Long id, @RequestBody MatchEntity requestedToUpdateMatchEntity) {
+        try {
+            MatchEntity updatedMatchEntity = matchService.updateMatch(id, requestedToUpdateMatchEntity);
+            return ResponseEntity.status(200).body(updatedMatchEntity);
+        } catch (ResponseStatusException ex) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
--- src/main/java/com/example/partidasdefutebol/service/MatchService.java
+++ src/main/java/com/example/partidasdefutebol/service/MatchService.java
@@ -2,6 +2,7 @@ package com.example.partidasdefutebol.service;
 
 import com.example.partidasdefutebol.entities.ClubEntity;
 import com.example.partidasdefutebol.entities.MatchEntity;
+import com.example.partidasdefutebol.entities.StadiumEntity;
 import com.example.partidasdefutebol.repository.MatchRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,21 +26,26 @@ public class MatchService {
     private StadiumService stadiumService;
 
     public MatchEntity createMatch(MatchEntity matchEntity) {
-        if (Objects.equals(matchEntity.getAwayClubId(), matchEntity.getHomeClubId())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+        isEachClubDifferent(matchEntity);
         ClubEntity homeClubEntity = clubService.findClubById(matchEntity.getHomeClubId());
         ClubEntity awayClubEntity = clubService.findClubById(matchEntity.getAwayClubId());
         clubService.wasClubCreatedBeforeGame(homeClubEntity, awayClubEntity, matchEntity.getMatchDate());
         clubService.isAnyOfClubsInactive(homeClubEntity, awayClubEntity);
         stadiumService.doesStadiumExist(matchEntity.getStadiumId());
+        stadiumIsFreeForMatchOnDay(matchEntity.getStadiumId(), matchEntity.getMatchDate());
         checkHomeClubRestPeriod(matchEntity.getHomeClubId(), matchEntity.getMatchDate());
         checkAwayClubRestPeriod(matchEntity.getAwayClubId(), matchEntity.getMatchDate());
 
         return matchRepository.save(matchEntity);
     }
 
-    public void stadiumIsFreeForMatchOnDay (Long stadiumId, LocalDateTime desiredMatchDate) throws ResponseStatusException {
+    public void isEachClubDifferent (MatchEntity matchEntity) {
+        if (Objects.equals(matchEntity.getAwayClubId(), matchEntity.getHomeClubId())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public void stadiumIsFreeForMatchOnDay(Long stadiumId, LocalDateTime desiredMatchDate) throws ResponseStatusException {
         if (!matchRepository.findByStadiumAndDate(stadiumId, desiredMatchDate).isEmpty()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
@@ -47,18 +53,31 @@ public class MatchService {
 
     public void checkHomeClubRestPeriod(Long homeClubId, LocalDateTime desiredMatchDate) throws ResponseStatusException {
         final int restPeriodInHoursIs = 48;
-        LocalDateTime lastGameForHomeClubWasIn = matchRepository.hoursSinceLastGameForHomeClub(homeClubId,desiredMatchDate);
+        LocalDateTime lastGameForHomeClubWasIn = matchRepository.hoursSinceLastGameForHomeClub(homeClubId, desiredMatchDate);
         if (Duration.between(lastGameForHomeClubWasIn, desiredMatchDate).toHours() < restPeriodInHoursIs) {
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
     }
 
-    public void checkAwayClubRestPeriod (Long homeClubId,LocalDateTime desiredMatchDate) throws ResponseStatusException {
+    public void checkAwayClubRestPeriod(Long homeClubId, LocalDateTime desiredMatchDate) throws ResponseStatusException {
         final int restPeriodInHoursIs = 48;
-        LocalDateTime lastGameForHomeClubWasIn = matchRepository.hoursSinceLastGameForHomeClub(homeClubId,desiredMatchDate);
+        LocalDateTime lastGameForHomeClubWasIn = matchRepository.hoursSinceLastGameForHomeClub(homeClubId, desiredMatchDate);
         if (Duration.between(lastGameForHomeClubWasIn, desiredMatchDate).toHours() < restPeriodInHoursIs) {
             throw new ResponseStatusException(HttpStatus.CONFLICT);
         }
+    }
+
+    public MatchEntity updateMatch(Long matchId, MatchEntity requestedToUpdateMatchEntity) {
+        isEachClubDifferent(requestedToUpdateMatchEntity);
+        matchRepository.findById(matchId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        MatchEntity existingMatchEntity = matchRepository.findById(matchId).get();
+        existingMatchEntity.setHomeClubId(requestedToUpdateMatchEntity.getHomeClubId());
+        existingMatchEntity.setAwayClubId(requestedToUpdateMatchEntity.getAwayClubId());
+        existingMatchEntity.setHomeClubNumberOfGoals(requestedToUpdateMatchEntity.getHomeClubNumberOfGoals());
+        existingMatchEntity.setAwayClubNumberOfGoals(requestedToUpdateMatchEntity.getAwayClubNumberOfGoals());
+        existingMatchEntity.setMatchDate(requestedToUpdateMatchEntity.getMatchDate());
+        existingMatchEntity.setStadiumId(requestedToUpdateMatchEntity.getStadiumId());
+        return matchRepository.saveAndFlush(existingMatchEntity);
     }
 
 }
